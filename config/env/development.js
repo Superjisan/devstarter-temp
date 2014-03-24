8ed3b9480235914de12309122d2cdb9c8b0c0381
disasteruss <brontosauruss@gmail.com>
2014-03-24 11:59:41 -0400

    test

--- config/env/development.js
+++ config/env/development.js
@@ -16,8 +16,8 @@ module.exports = {
         callbackURL: 'http://localhost:3000/auth/twitter/callback'
     },
     github: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
+        clientID: '8384af5c1911750906c5',
+        clientSecret: '9dc50a5a5b8ccff5ce76ec6ef47d613a5c2b3709',
         callbackURL: 'http://localhost:3000/auth/github/callback'
     },
     google: {
