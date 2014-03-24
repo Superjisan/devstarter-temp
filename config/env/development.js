96f0cc7f233af3bfc07d01e5bd98b4f3de69c0a8
Migi Domingo <migi.domingo@gmail.com>
2014-03-24 13:44:20 -0400

    clean up unnecessary components

--- config/env/development.js
+++ config/env/development.js
@@ -5,26 +5,26 @@ module.exports = {
     app: {
         name: 'Dev Starter - Dynamic resume site for Junior Developers'
     },
-    facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/facebook/callback'
-    },
-    twitter: {
-        clientID: 'CONSUMER_KEY',
-        clientSecret: 'CONSUMER_SECRET',
-        callbackURL: 'http://localhost:3000/auth/twitter/callback'
-    },
+    // facebook: {
+    //     clientID: 'APP_ID',
+    //     clientSecret: 'APP_SECRET',
+    //     callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    // },
+    // twitter: {
+    //     clientID: 'CONSUMER_KEY',
+    //     clientSecret: 'CONSUMER_SECRET',
+    //     callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    // },
     github: {
         clientID: '8384af5c1911750906c5',
         clientSecret: '9dc50a5a5b8ccff5ce76ec6ef47d613a5c2b3709',
         callbackURL: 'http://localhost:3000/auth/github/callback'
     },
-    google: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
-        callbackURL: 'http://localhost:3000/auth/google/callback'
-    },
+    // google: {
+    //     clientID: 'APP_ID',
+    //     clientSecret: 'APP_SECRET',
+    //     callbackURL: 'http://localhost:3000/auth/google/callback'
+    // },
     linkedin: {
         clientID: '77qy5d9cyaxg74',
         clientSecret: 'DIfXoYralsxVMTZN',
