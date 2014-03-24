accbb52d1499f145db171d102c640dbc283276f5
Jisan Zaman <jzthegreat@gmail.com>
2014-03-24 11:20:26 -0400

    linkedin works

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean-dev',
+    db: 'mongodb://localhost/devstarter',
     app: {
-        name: 'MEAN - A Modern Stack - Development'
+        name: 'Dev Starter - Dynamic resume site for Junior Developers'
     },
     facebook: {
         clientID: 'APP_ID',
@@ -16,8 +16,8 @@ module.exports = {
         callbackURL: 'http://localhost:3000/auth/twitter/callback'
     },
     github: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
+        clientID: '8384af5c1911750906c5',
+        clientSecret: '9dc50a5a5b8ccff5ce76ec6ef47d613a5c2b3709',
         callbackURL: 'http://localhost:3000/auth/github/callback'
     },
     google: {
@@ -26,8 +26,8 @@ module.exports = {
         callbackURL: 'http://localhost:3000/auth/google/callback'
     },
     linkedin: {
-        clientID: 'API_KEY',
-        clientSecret: 'SECRET_KEY',
+        clientID: '77qy5d9cyaxg74',
+        clientSecret: 'DIfXoYralsxVMTZN',
         callbackURL: 'http://localhost:3000/auth/linkedin/callback'
     }
 };
