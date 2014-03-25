4f52d06ed39665cb5e3c27cc7502608df894cbc3
Migi Domingo <migi.domingo@gmail.com>
2014-03-24 23:14:52 -0400

    working linkedin and github code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var authorization = require('./middlewares/authorization');
+
 module.exports = function(app) {
-    
+
     // Home route
     var index = require('../controllers/index');
     app.get('/', index.render);
+    app.get('/test', authorization.requiresLogin, index.test_route);
 
 };
--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,21 @@ var mongoose = require('mongoose'),
     // GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
     LinkedinStrategy = require('passport-linkedin').Strategy,
     User = mongoose.model('User'),
+    _ = require("lodash"),
     config = require('./config');
 
+
+var parseLinkedIn = function(profile) {
+	return {
+    name: profile.displayName,
+    headline: profile._json.headline,
+    email: profile._json.emailAddress,
+    work_experiences: profile._json.positions.values,
+    educations: profile._json.educations.values,
+    auth_methods: [{provider: 'linkedin', providerId: profile._json.id}],
+    linkedin: profile._json
+  };
+};
 module.exports = function(passport) {
 
     // Serialize the user id to push into the session
@@ -119,36 +132,38 @@ module.exports = function(passport) {
 
     // Use github strategy
     passport.use(new GitHubStrategy({
-            clientID: config.github.clientID,
-            clientSecret: config.github.clientSecret,
-            callbackURL: config.github.callbackURL,
-            passReqToCallback: true
-        },
-        function(req, accessToken, refreshToken, profile, done) {
-        	console.log(profile);
-            User.findOne({
-                'github.id': profile.id
-            }, function(err, github) {
-                if (!github) {
-                    // console.log(req.user)
-                 User.findOne({ 'linkedin.id' : req.user.linkedin.id},
-                    function(err, user){
-                      if(err) console.log(err);
-                        user.github = profile._json;
-                        console.log(user);
-                        user.save(function(err){
-                            if(err) console.log(err);
-                            return done(err, github)
-                        }
-                      )
-                    }
-                 )}
-                else {
-                    return done(err, github);
-                }
-            });
+          clientID: config.github.clientID,
+          clientSecret: config.github.clientSecret,
+          callbackURL: config.github.callbackURL,
+          passReqToCallback: true
+      },
+      function(req, accessToken, refreshToken, profile, done) {
+      	console.log(refreshToken);
+          User.findOne({
+          	'auth_methods.provider': "github",
+          	'auth_methods.providerId': profile.id
+          }, function(err, github) {
+            if (!github) {
+              User.findOne({ 'linkedin.id' : req.user.linkedin.id}, function(err, user){
+                if(err) console.log(err);
+                user.github = profile._json;
+                user.auth_methods.push({
+                	provider: 'github',
+                	providerId: profile.id,
+                	accessToken: accessToken,
+                	refreshToken: refreshToken
+                });
+                user.save(function(err){
+                  if(err) console.log(err);
+                  return done(err, github)
+                });
+              });
+            } else {
+              return done(err, github);
+            }
+          });
         }
-    ));
+      ));
 
     // Use google strategy
     // passport.use(new GoogleStrategy({
@@ -189,15 +204,12 @@ module.exports = function(passport) {
         function(accessToken, refreshToken, profile, done) {
         	console.log(profile);
             User.findOne({
-                'linkedin.id': profile.id
+                'authMethods.providerId': profile.id,
+                'authMethods.provider'	:	"linkedin"
             }, function(err, user) {
                 if (!user) {
-                    user = new User({
-                        name: profile.displayName,
-                        email: profile._json.emailAddress,
-                        provider: 'linkedin',
-                        linkedin: profile._json
-                    });
+                	var linkedInFields = parseLinkedIn(profile);
+                    user = new User(linkedInFields);
                     user.save(function(err) {
                         if (err) console.log(err);
                         return done(err, user);
