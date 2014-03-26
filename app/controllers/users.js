085475003c8ff8af959dc0a388655044da467112
disasteruss <brontosauruss@gmail.com>
2014-03-25 20:20:14 -0400

    fixed stuff

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -126,8 +126,8 @@ exports.admin = function(req, res) {
  * Show user profile
  */
 exports.profile = function(req, res) {
-	console.log(req.user.name);
-	User.findOne({name: req.user.name}, function(err, user) {
+	console.log(req.params.name);
+	User.findOne({name: req.params.name}, function(err, user) {
 		res.render('profile', {
 			user: user
 		});
