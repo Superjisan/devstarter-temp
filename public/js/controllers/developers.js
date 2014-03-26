6c4722bdaad4eaa410612ef6c0dc3ba15bbbb2ea
Jisan Zaman <jzthegreat@gmail.com>
2014-03-26 14:41:36 -0400

    fixed swig bracket conflict

--- public/js/controllers/developers.js
+++ public/js/controllers/developers.js
@@ -1,14 +1,25 @@
 'use strict';
 
-angular.module('mean.developers').controller('DevelopersController',
+angular.module('mean.developers')
+
+.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+})
+
+.controller('DevelopersController',
 	['$scope',
 	'$stateParams',
 	'$location',
 	 '$window',
+
 	'Global',
-	function($scope, $stateParams, $location, $window, Global) {
+
+	function($scope, $stateParams, $location, $window,  Global) {
 		$scope.global = Global;
 		$scope.limitct = 10;
 
-		$scope.developers = $window.developers
+		$scope.developers = $window.developers;
+
+
 	}]);
