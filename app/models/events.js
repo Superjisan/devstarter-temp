0502d1bf683070dbcb00a8ed319754ac7a1fdd23
Migi Domingo <migi.domingo@gmail.com>
2014-03-27 15:26:04 -0400

    added events.js to models

---
+++ app/models/events.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ClicksSchema = new Schema({
+	url: String,
+	time_clicked: {type: Date}
+})
+
+var EventsSchema = new Schema({
+	visiting_user: {id: {type: Schema.Types.ObjectId, ref: 'Users'},
+									name: {type: String, ref: 'Users'},
+									organization: {type: String, ref: 'Users'}},
+	user_visited: {id: {type: Schema.Types.ObjectId, ref: 'Users'},
+								name: {type: String, ref: 'Users'},
+								organization: {type: String, ref: 'Users'}},
+	start_time: {type: Date},
+	end_time: {type: Date},
+	clicks: [ClicksSchema]
+});
+
+exports.ClicksSchema = ClicksSchema;
+exports.EventsSchema = EventsSchema;
