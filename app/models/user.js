9f9a39ffee2b12aacae4cecda9e67fc44fd711ed
disasteruss <brontosauruss@gmail.com>
2014-03-27 14:05:04 -0400

    working on favorites page

--- app/models/user.js
+++ app/models/user.js
@@ -12,24 +12,25 @@ var mongoose = require('mongoose'),
  */
 var UserSchema = new Schema({
 	name: {
-        type: String,
-        required: true
-    },
-    email: String,
-    provider: String,
-    roles: [],
-    github: {},
-    linkedin: {},
-    educations: [EducationSchema],
-    work_experiences: [WorkExperienceSchema],
-    auth_methods: [{provider: String, providerId: String, accessToken: String, refreshToken: String}]
+		type: String,
+		required: true
+	},
+	interested: Array,
+	email: String,
+	provider: String,
+	roles: [],
+	github: {},
+	linkedin: {},
+	educations: [EducationSchema],
+	work_experiences: [WorkExperienceSchema],
+	auth_methods: [{provider: String, providerId: String, accessToken: String, refreshToken: String}]
 });
 
 
 UserSchema.statics.load = function(id, cb) {
-    this.findOne({
-        _id: id
-    }).exec(cb);
+	this.findOne({
+		_id: id
+	}).exec(cb);
 };
 
 
@@ -49,20 +50,20 @@ UserSchema.statics.load = function(id, cb) {
  * Validations
  */
 var validatePresenceOf = function(value) {
-    return value && value.length;
+	return value && value.length;
 };
 
 // The 4 validations below only apply if you are signing up traditionally.
 UserSchema.path('name').validate(function(name) {
-    // If you are authenticating by any of the oauth strategies, don't validate.
-    if (!this.provider) return true;
-    return (typeof name === 'string' && name.length > 0);
+		// If you are authenticating by any of the oauth strategies, don't validate.
+	if (!this.provider) return true;
+	return (typeof name === 'string' && name.length > 0);
 }, 'Name cannot be blank');
 
 UserSchema.path('email').validate(function(email) {
-    // If you are authenticating by any of the oauth strategies, don't validate.
-    if (!this.provider) return true;
-    return (typeof email === 'string' && email.length > 0);
+		// If you are authenticating by any of the oauth strategies, don't validate.
+	if (!this.provider) return true;
+		return (typeof email === 'string' && email.length > 0);
 }, 'Email cannot be blank');
 
 // UserSchema.path('username').validate(function(username) {
