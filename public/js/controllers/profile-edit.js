bc2ff65f4dc1a2d42229d450d07da84131a89213
David Yang <david.g.yang@gmail.com>
2014-03-28 22:23:20 -0400

    get rid of annoying jshint and fix masonry bug

--- public/js/controllers/profile-edit.js
+++ public/js/controllers/profile-edit.js
@@ -2,32 +2,31 @@
 
 angular.module('mean.profile-edit')
   .controller('ProfileEditCtrl', ['$scope','Global', 'ProfileEditSrvc', 'Work', 'Education', 'Project',
-  	function ($scope, Global, ProfileEditSrvc, Work, Education, Project) {
-      $scope.global = Global;
+    function ($scope, Global, ProfileEditSrvc, Work, Education, Project) {
+      // $scope.global = Global;
 
       ProfileEditSrvc.getProfile(function(data) {
-      	console.log(data);
-      	$scope.user = data;
-      	$scope.skills = data.linkedin.skills.values;
+        $scope.user = data;
+        $scope.skills = data.linkedin.skills.values;
       });
 
       $scope.submitForm = function() {
-      	ProfileEditSrvc.editProfile($scope.user, function(data) {
-      		$scope.user = data;
-      	})
-      }
+        ProfileEditSrvc.editProfile($scope.user, function(data) {
+          $scope.user = data;
+        })
+      };
 
       $scope.removeEducation = function(index) {
-      	var removedEducation = $scope.user.educations.splice(index, 1);
-      	var education = new Education(removedEducation[0]);
-      	education.$remove();
+        var removedEducation = $scope.user.educations.splice(index, 1);
+        var education = new Education(removedEducation[0]);
+        education.$remove();
       };
       $scope.addEducation = function() {
-      	var education = new Education($scope.new_education);
-      	education.$save(function() {
-      		$scope.user.educations.unshift($scope.new_education);
-      		$scope.new_education = {};
-      	})
+        var education = new Education($scope.new_education);
+        education.$save(function() {
+          $scope.user.educations.unshift($scope.new_education);
+          $scope.new_education = {};
+        })
       }
 
       $scope.removeWork = function(index) {
@@ -36,10 +35,10 @@ angular.module('mean.profile-edit')
         work.$remove();
       };
       $scope.addWork = function() {
-      	console.log($scope.new_work.startDate);
-      	var new_work = angular.copy($scope.new_work);
-      	new_work.startDate = convertToDate($scope.new_work.startDate);
-      	new_work.endDate = convertToDate($scope.new_work.endDate);
+        console.log($scope.new_work.startDate);
+        var new_work = angular.copy($scope.new_work);
+        new_work.startDate = convertToDate($scope.new_work.startDate);
+        new_work.endDate = convertToDate($scope.new_work.endDate);
         var work = new Work(new_work);
         work.$save(function() {
           $scope.user.work_experiences.unshift(new_work);
@@ -48,55 +47,55 @@ angular.module('mean.profile-edit')
       };
 
       function convertToDate(dateObj) {
-      	if (!dateObj) {
-      		return undefined;
-      	}
-      	// var monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Nov", "Dec"];
-      	numMonth = dateObj.getMonth()+1;
-      	return { "year": dateObj.getFullYear(), "month": numMonth };
+        if (!dateObj) {
+          return undefined;
+        }
+        // var monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Nov", "Dec"];
+        // var numMonth = monthArr[dateObj.getMonth()+1];
+        return { "year": dateObj.getFullYear(), "month": dateObj.getMonth()+1 };
       }
 
       $scope.removeProject = function(index) {
-      	var removedProject = $scope.user.projects.splice(index, 1);
-      	var project = new Project(removedProject[0]);
-      	project.$remove();
+        var removedProject = $scope.user.projects.splice(index, 1);
+        var project = new Project(removedProject[0]);
+        project.$remove();
       }
 
       $scope.addProject = function() {
-      	$scope.new_project.tags = $scope.projectTags;
-      	var project = new Project($scope.new_project);
-      	project.$save(function() {
-      		$scope.user.projects.unshift($scope.new_project);
-      		$scope.new_project = {};
-      		$scope.projectTags = [];
-      	})
+        $scope.new_project.tags = $scope.projectTags;
+        var project = new Project($scope.new_project);
+        project.$save(function() {
+          $scope.user.projects.unshift($scope.new_project);
+          $scope.new_project = {};
+          $scope.projectTags = [];
+        })
       }
 
       $scope.removeTag = function(index) {
-      	$scope.skills.splice(index, 1);
+        $scope.skills.splice(index, 1);
       }
 
       $scope.addTag = function(event) {
-      	if(event.which === 13) {
-	      	$scope.skills.push({skill: {name: $scope.current_tag}});
-	      	$scope.current_tag = "";
-	      	event.preventDefault();
-      	}
+        if(event.which === 13) {
+          $scope.skills.push({skill: {name: $scope.current_tag}});
+          $scope.current_tag = "";
+          event.preventDefault();
+        }
       };
 
       $scope.projectTags = [];
 
 
       $scope.removeProjectTag = function(index) {
-      	$scope.projectTags.splice(index, 1);
+        $scope.projectTags.splice(index, 1);
       }
 
       $scope.addProjectTag = function(event) {
-      	if(event.which === 13) {
-      		$scope.projectTags.push($scope.currentTag);
-      		$scope.currentTag = "";
-      		event.preventDefault();
-      	}
+        if(event.which === 13) {
+          $scope.projectTags.push($scope.currentTag);
+          $scope.currentTag = "";
+          event.preventDefault();
+        }
       }
 
     }
