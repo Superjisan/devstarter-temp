8fb65b89fe2df3afde07673c3460c2e6858d8c4c
Howard Kim <howardjink@gmail.com>
2014-03-30 08:01:00 -0400

    Made image-holders links and removed 'Add Picture' buttons. Reverted background-color to original template's color. (Subject to change.) Experimenting with fixed sidebar to figure what the buttons should look like.

--- public/js/config.js
+++ public/js/config.js
@@ -20,8 +20,8 @@
 // ]);
 
 //Setting HTML5 Location Mode
-angular.module('mean').config(['$locationProvider',
-	function($locationProvider) {
-		$locationProvider.hashPrefix('!');
-	}
-]);
+// angular.module('mean').config(['$locationProvider',
+// 	function($locationProvider) {
+// 		$locationProvider.hashPrefix('!');
+// 	}
+// ]);
