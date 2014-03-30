629d160d2a6f64329fc60150473c2b66b32b03d3
David Yang <david.g.yang@gmail.com>
2014-03-30 00:25:58 -0400

    linkedin config

--- config/env/production.js
+++ config/env/production.js
@@ -34,6 +34,6 @@ module.exports = {
         clientID: process.env.LINKEDIN_KEY,
         clientSecret: process.env.LINKEDIN_SECRET,
         callbackURL: 'http://hire.fullstackacademy.com/auth/linkedin/callback',
-        profileFields: ['id','first-name', 'last-name', 'email-address', 'headline', 'summary', 'specialties', 'picture-url', 'site-standard-profile-request', 'primary-twitter-account', 'interests', 'languages', 'skills', 'certifications', 'educations', 'courses', 'volunteer', 'three-current-positions', 'three-past-positions', 'num-recommenders', 'recommendations-received', 'date-of-birth', 'related-profile-views', 'honors-awards']
+        profileFields: ['id','first-name', 'last-name', 'email-address', 'headline', 'summary', 'specialties', 'picture-url', 'site-standard-profile-request', 'phone-numbers', 'primary-twitter-account', 'interests', 'languages', 'skills', 'certifications', 'educations', 'courses', 'volunteer', 'positions', 'num-recommenders', 'recommendations-received', 'date-of-birth', 'related-profile-views', 'honors-awards']
     }
 };
