73114ad1cee090f3ce5cdbdf28d402ad92a902ee
David Yang <david.g.yang@gmail.com>
2014-03-31 00:23:09 -0400

    fix masonry

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'mean.system', 'mean.developers', 'mean.tracking', 'mean.articles', 'mean.profile-edit','mean.favorites', 'ngAnimate'])
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'mean.system', 'mean.developers', 'mean.tracking', 'wu.masonry', 'mean.articles', 'mean.profile-edit','mean.favorites', 'ngAnimate'])
   .config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
